bbeffcbfa5d30106acfcdeebdecd197dac4b1b84
kpsr01 <pranavsuhasreddykonduru@gmail.com>
2025-10-19 22:09:13 +0530

    added navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src="/logo.jpg" alt="ReactFood Logo" />
+        <h1>REACTFOOD</h1>
+      </div>
+      <nav>
+        <button className="text-button">Cart (0)</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
