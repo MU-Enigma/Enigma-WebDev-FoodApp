946c93f21adbc8b8ee3518da0a058fe9b22687b7
LazyHusky26 <aarnavthelegend26@gamil.com>
2025-10-20 12:44:50 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,36 @@
-import { CartContextProvider } from './store/CartContext.jsx';
 import Navbar from './components/Navbar.jsx';
 import Meals from './components/Meals.jsx';
 import Particles from './components/Particles.jsx';
 
 function App() {
   return (
-    <CartContextProvider>
-      <div style={{ position: 'relative', minHeight: '100vh' }}>
-        <div style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          zIndex: -10
-        }}>
-          <Particles
-            particleColors={['#ffc404', '#ffab04', '#d9e2f1']}
-            particleCount={100}
-            particleSpread={10}
-            speed={0.1}
-            particleBaseSize={200}
-            moveParticlesOnHover={true}
-            alphaParticles={false}
-            disableRotation={false}
-          />
-        </div>
-        <div style={{ position: 'relative', zIndex: 1 }}>
-          <Navbar />
-        </div>
-        <main style={{ position: 'relative', zIndex: 1 }}>
-          <Meals />
-        </main>
+    <div style={{ position: 'relative', minHeight: '100vh' }}>
+      <div style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: -10
+      }}>
+        <Particles
+          particleColors={['#ffc404', '#ffab04', '#d9e2f1']}
+          particleCount={100}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={200}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
       </div>
-    </CartContextProvider>
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <Navbar />
+      </div>
+      <main style={{ position: 'relative', zIndex: 1 }}>
+        <Meals />
+      </main>
+    </div>
   );
 }
 
