8d889d6ae2f04c06bb13648c0cafafd5dafaabda
LazyHusky26 <aarnavthelegend26@gamil.com>
2025-10-20 17:01:29 +0530

    upd

--- src/components/Meal-card.jsx
+++ src/components/Meal-card.jsx
@@ -1,10 +1,19 @@
+import ShinyText from './shiny-text/ShinyText.jsx';
+
 const MealCard = ({ meal }) => {
   return (
     <li className="meal-item">
       <article>
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
-          <h3>{meal.name}</h3>
+          <h3>
+            <ShinyText 
+              text={meal.name} 
+              disabled={false} 
+              speed={3} 
+              className="meal-name-shiny"
+            />
+          </h3>
           <p className="meal-item-price">${meal.price}</p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
