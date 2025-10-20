34844e91805fdae5ffcc5885797a27f4dd9e7b8e
LazyHusky26 <aarnavthelegend26@gamil.com>
2025-10-20 12:43:25 +0530

    bg

--- src/components/Meal-card.jsx
+++ src/components/Meal-card.jsx
@@ -1,19 +1,19 @@
-const MealCard = ({meal}) => {
+const MealCard = ({ meal }) => {
   return (
-      <li className="meal-item">
-        <article>
-          <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
-          <div>
-            <h3>{meal.name}</h3>
-            <p className="meal-item-price">${meal.price}</p>
-            <p className="meal-item-description">{meal.description}</p>
-          </div>
-          <p className="meal-item-actions">
-            <button className="button">Add to Cart</button>
-          </p>
-        </article>
-      </li>
-    );
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">${meal.price}</p>
+          <p className="meal-item-description">{meal.description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <button className="button">Add to Cart</button>
+        </p>
+      </article>
+    </li>
+  );
 }
 
 export default MealCard;
